6ffbafe1598d4acd25edd367d676eca52953f6b8
Ilham <Azalam Ilham>
2025-04-16 09:29:53 +0100

    modification de repository

--- src/main/java/ma/ac/bouzekraoui/Main.java
+++ src/main/java/ma/ac/bouzekraoui/Main.java
@@ -3,5 +3,7 @@ package ma.ac.bouzekraoui;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bonjour, C'est Bouzekraoui Fatima Ezzahrae!");
+        //
+        System.out.println("Bonjour Fati .C'est Ilham .");
     }
 }
